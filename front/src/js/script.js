bc7e260d1836ce614bb63f1670fa11d2c1ff2d7a
SSurikkiller <190107046@stu.sdu.edu.kz>
2022-04-30 20:31:46 +0600

    Push.

--- front/src/js/script.js
+++ front/src/js/script.js
@@ -15,9 +15,7 @@ $(document).ready(function () {
 		navigation.removeClass('nav_active');
 	});
 });
-
 let map;
-
 function initMap() {
 	map = new google.maps.Map(document.getElementById("map"), {
 		center: { lat: -34.397, lng: 150.644 },
